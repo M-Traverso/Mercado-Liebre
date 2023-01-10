a7e5bf52288004e99d12c7550b556a28a8b3f38c
mtraver <m.traverso96@gmail.com>
2023-01-09 21:24:47 -0300

    fist commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3003, () => {
+    console.log('Servidor corriendo en el puerto 3003');
+});
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+})
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+})
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+})
